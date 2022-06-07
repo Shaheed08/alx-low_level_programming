d63355932dd6a4f8a56bbb9f04210b7a90c7e1c4
Shaheed08 <daleel5880@gmail.com>
2022-06-07 14:28:48 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,18 @@
 #include "main.h"
+
 /**
  * * main - Prints _putchar as a message.
  * * Return: Always 0 (Success)
  **/
 int main(void)
+
 {
 	int str[] = {109, 97, 105, 110,};
+	
 	int count, sz;
+	
 	sz = sizeof(str) / sizeof(int);
+	
 	for (count = 0; count < sz; count++)
 	{
 		_putchar(str[count]);
